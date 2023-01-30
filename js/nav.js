1478463d4187743b2fb5de30de6eda644bd86bd7
PhuDat Tran <cctalk1114@r6j0.onmicrosoft.com>
2023-01-30 20:34:41 +0700

    ok

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,19 @@
-let myNav = `<nav class="navbar navbar-expand-lg ">
-<div class="container">
+let myNav = `<nav class="navbar navbar-expand-lg">
+<div class="container visually-hidden mt-3" id="searchbar-nav">
+    <div class="row w-100 text-center">
+        <div class="col-2">
+            <button onclick="backNav()" class="btn btn-outline-warning"><i
+                    class="fa-solid fa-arrow-left"></i></button>
+        </div>
+        <div class="col-8">
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+        </div>
+        <div class="col-2">
+            <button class="btn btn-outline-warning"><i class="fa-solid fa-magnifying-glass"></i></button>
+        </div>
+    </div>
+</div>
+<div class="container" id="normal-nav">
     <a class="navbar-brand d-none d-lg-block" href="#"><img class="rounded-circle" height="50"
             src="../assets/logo.svg" alt="logo">
         NetUniverse</a>
@@ -31,41 +45,42 @@ let myNav = `<nav class="navbar navbar-expand-lg ">
         </ul>
     </div>
     <div class="collapse navbar-collapse justify-content-end" id="navbar">
-        <ul class="navbar-nav me-2">
+        <ul class="navbar-nav gap-2">
             <li class="nav-item">
-                <a class="btn-outline-light btn me-2 mb-2  text-decoration-none"
-                    href="../screen/Login.html#login">
+                <a class="btn-outline-light btn text-decoration-none" href="../screen/Login.html#login">
                     Login</a>
             </li>
             <li class="nav-item ">
-                <a class="btn-warning btn text-decoration-none me-2 mb-2" href="../screen/SignUp.html#signup">
+                <a class="btn-warning btn text-decoration-none" href="../screen/SignUp.html#signup">
                     Sign up</a>
             </li>
-            <li class="nav-item ">
-                <button class="btn me-2 mb-2"><i class="fa-solid fa-moon fa-2x text-warning"></i></button>
+            <li class="nav-item">
+                <button class="btn btn-outline-warning" onclick="toSearchBar()"><i
+                        class="fa-solid fa-magnifying-glass"></i></button>
             </li>
-            <li class="nav-item dropdown d-none d-lg-block">
-                <div class="btn-group">
-                    <button class="btn text-white mb-2" data-bs-toggle="dropdown" aria-expanded="false"><i
-                            class="fa-solid fa-earth-asia fa-2x"></i></button>
-                    <button class="btn text-white mb-2 dropdown-toggle dropdown-toggle-split"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        <span class="visually-hidden">Toggle Dropdown</span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <p class="dropdown-item disabled">Language</p>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="#">vi - Tiếng việt</a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="#">en - English</a>
-                        </li>
-                    </ul>
+            <li class="nav-item dropdown dropstart">
+                <button class="btn btn-outline-warning" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false"><i class="fa-solid fa-gear"></i></button>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a href="#" class="dropdown-item disabled">
+                            <i class="fa-solid fa-language "></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">vi - Tiếng việt</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">en - English</a>
+                    </li>
+                    <hr>
+                    <li>
+                        <a class="dropdown-item" href="#"><i class="fa-solid fa-sun"></i> Giao diện - Sáng</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#"><i class="fa-solid fa-moon"></i> Giao diện - Tối</a>
+                    </li>
+                </ul>
             </li>
         </ul>
     </div>
