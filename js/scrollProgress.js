e9f7a86c35930b850ea55781393fd73be0063551
PhuDat Tran <cctalk1114@r6j0.onmicrosoft.com>
2023-02-16 05:33:22 +0700

    update Progress update route update css update index file

--- js/scrollProgress.js
+++ js/scrollProgress.js
@@ -1,6 +1,12 @@
 let processScroll = () => {
-    let scrollPercent = (window.pageYOffset / (document.body.querySelector('#blog-content-here').offsetHeight - (window.innerHeight - (document.body.querySelector('#footerDotnet').offsetHeight - document.body.querySelector('#other-content').offsetHeight))) * 100)
-    document.getElementById("progress-bar").style.width = scrollPercent + '%';
-    console.log(scrollPercent)
+    if (window.location.hash.split('#').at(-1) === 'blogReadingContent') {
+        let scrollPercent = (window.pageYOffset / (document.body.querySelector('#blog-content-here').offsetHeight - (window.innerHeight - (document.body.querySelector('#footerDotnet').offsetHeight - document.body.querySelector('#other-content').offsetHeight))) * 100);
+
+        document.getElementById("progress-bar").style.width = scrollPercent + '%';
+    }
+    else {
+        document.getElementById("progress-bar").style.width = 0
+    }
+    // console.log(scrollPercent)
 }
 document.addEventListener('scroll', processScroll);
