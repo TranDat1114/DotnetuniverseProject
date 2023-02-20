2ada731ca2a0b06453fcc258fb42784c346781e6
PhuDat Tran <cctalk1114@r6j0.onmicrosoft.com>
2023-02-20 20:21:10 +0700

    ok

--- js/scrollProgress.js
+++ js/scrollProgress.js
@@ -1,5 +1,6 @@
 let processScroll = () => {
-    if (window.location.hash.split('#').at(-1) === 'blogReadingContent') {
+    var href_link = window.location.hash.split('#');
+    if (href_link.at(-1) === 'blogReadingContent' && href_link.at(-2) != "!/404notfound") {
         let scrollPercent = (window.pageYOffset / (document.body.querySelector('#blog-content-here').offsetHeight - (window.innerHeight - (document.body.querySelector('#footerDotnet').offsetHeight - document.body.querySelector('#other-content').offsetHeight))) * 100);
 
         document.getElementById("progress-bar").style.width = scrollPercent + '%';
---
+++ js/whenHrefChange.js
@@ -0,0 +1,3 @@
+
+var tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+var tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
